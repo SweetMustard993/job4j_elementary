332add0d62b3c8f4082f5842d4f6f50d59360fa0
SweetMustard993 <bogdanasdaq@gmail.com>
2021-11-12 21:56:29 +0300

    задание на уроке

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,9 +2,8 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
+        System.out.println("Bogdan Mazurenko");
+        System.out.println("12.08.1993");
 
-        for (int i=2;i<10; i++){
-            System.out.println("1*"+i+"="+1*i);
-        }
     }
 }
