23b58c6d784a411e64b2e0d65267885ace11ea2b
SweetMustard993 <bogdanasdaq@gmail.com>
2021-12-05 18:45:20 +0300

    6.0.3.1. Массивы и цикл for-each [#333581]

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -6,8 +6,8 @@ public class ArrayLoop {
         for (int i = 0; i < arrayLoop.length; i++) {
             arrayLoop[i] = i * 2 + 3;
         }
-        for (int i = 0; i < arrayLoop.length; i++) {
-            System.out.println("Значение ячейки c с индексом [" + i + "] - " + arrayLoop[i]);
+        for (int element : arrayLoop) {
+            System.out.println(element);
 
         }
     }
