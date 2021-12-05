12b6c47099ce3b3931d6202313a124ff2f08e9b0
SweetMustard993 <bogdanasdaq@gmail.com>
2021-12-05 18:36:23 +0300

    6.0.1. Объявление массива. [#3613 #235142]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,12 +2,12 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] arrayShort = new short[10];
-        System.out.println("Длина массива arrayShort [" + arrayShort.length + "]");
-        String[] arrayString = new String[100500];
-        System.out.println("Длина массива arrayString [" + arrayString.length + "]");
-        float[] arrayFloat = new float[40];
-        System.out.println("Длина массива arrayFloat [" + arrayString.length + "]");
+        short[] ages = new short[10];
+        System.out.println("Длина массива arrayShort [" + ages.length + "]");
+        String[] surnames = new String[100500];
+        System.out.println("Длина массива arrayString [" + surnames.length + "]");
+        float[] prices = new float[40];
+        System.out.println("Длина массива arrayFloat [" + prices.length + "]");
         String[] names = new String[4];
         names[0] = "Петр";
         names[1] = "Богдан";
