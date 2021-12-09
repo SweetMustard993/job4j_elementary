fbca2acbeb4db8c0870dd97d86b2a0df994306c6
SweetMustard993 <bogdanasdaq@gmail.com>
2021-12-09 22:08:08 +0300

    6.4. Слово начинается с ... [#41585 #236608]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -5,7 +5,6 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
-            System.out.println("значение индекса " + i);
             if (pref[i] != word[i]) {
                 result = false;
                 break;
